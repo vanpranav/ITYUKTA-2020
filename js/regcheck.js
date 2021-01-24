2bce7ccb0998f83fac9ee03fa55e319e9d442f95
V <pranavanandav@gmail.com>
2021-01-24 20:39:07 +0530

    Stricter Phone Number check

--- js/regcheck.js
+++ js/regcheck.js
@@ -1,115 +1,171 @@
-function my() {
+function my() 
+{
 	alert('Confirm your details');
+
 	var total = 0;
 	var a = 0;
-	var events1 = 50;
-	var events2 = 100;
-	var workshops1 = 700;
-	var workshops2 = 500;
-	var workshops3 = 350;
+
+	var event1 = 50;
+	var event2 = 100;
+
+	var workshop1 = 700;
+	var workshop2 = 500;
+	var workshop3 = 350;
+
 	var name = document.forms['regform']['name'].value;
 	var namelen = name.length;
+	
 	var col = document.forms['regform']['col'].value;
 	var collen = col.length;
+
 	var phone = document.forms['regform']['phone'].value;
 	var phonelen = phone.length;
 
-	if (name) {
-		if (namelen < 3) {
+	if (name) 
+	{
+		if (namelen < 3) 
+		{
 			window.alert('Enter atleast three characters in name field!!');
 			return false;
 		}
+
 		var k = 0;
-		for (i = 0; i < namelen; i++) {
+
+		for (i = 0; i < namelen; i++) 
+		{
 			l = /[a-z\s]/i;
-			if (l.exec(name[i])) {
+			if (l.exec(name[i])) 
+			{
 				if (name[i] == ' ') k = parseInt(k) + 1;
 				else k = 0;
-				if (k > 2) {
+
+				if (k > 2) 
+				{
 					alert('Name cannot contain more spaces!!');
 					return false;
 				}
 				continue;
-			} else {
+			} 
+			else 
+			{
 				window.alert('Name cannot contain special characters or numbers!!');
 				return false;
-			} //else
-		} //for
-	} //name
+			}
+		}
+	}
 
-	if (col) {
-		if (collen < 3) {
+	if (col) 
+	{
+		if (collen < 3) 
+		{
 			window.alert('Enter atleast Three characters in college name field!!');
 			return false;
 		}
+
 		var k = 0;
-		for (i = 0; i < collen; i++) {
+
+		for (i = 0; i < collen; i++) 
+		{
 			l = /[a-z\s]/i;
-			if (l.exec(col[i])) {
+
+			if (l.exec(col[i]))
+			{
 				if (col[i] == ' ') k = parseInt(k) + 1;
 				else k = 0;
-				if (k > 3) {
+
+				if (k > 3)
+				{
 					alert('College Name cannot contain more spaces!!');
 					return false;
 				}
 				continue;
-			} else {
+			} 
+			else 
+			{
 				window.alert('College Name cannot contain special characters or numbers!!');
 				return false;
-			} //else
-		} //for
-	} //name
+			}
+		}
+	}
 
-	if (phone) {
-		var phoneno = /^\d{10}$/;
-		if (!phone.match(phoneno)) {
-			alert('Phone Number can contain only 10 digits and numbers');
+	if (phone) 
+	{
+		var num = /^\d+$/;
+		var pattern = /^[6-9]\d{9}$/;
+
+		if (!phone.match(num))
+		{
+			alert('Phone Number can contain only numbers');
+			return false;
+		}
+		else if(phonelen != 10)
+		{
+			alert('Phone Number must contain 10 digits');
+			return false;
+		}
+		else if (!phone.match(pattern))
+		{
+			alert('Phone number should begin with either 6/7/8/9');
 			return false;
 		}
 	}
 
-	if (document.forms['regform']['CB'].checked) {
+	if (document.forms['regform']['CB'].checked)
+	{
 		a = 1;
-		total += events2;
+		total += event2;
 	}
 
-	if (document.forms['regform']['STC'].checked) {
+	if (document.forms['regform']['STC'].checked)
+	{
 		a = 1;
-		total += events1;
+		total += event1;
 	}
 
-	if (document.forms['regform']['ST'].checked) {
+	if (document.forms['regform']['ST'].checked)
+	{
 		a = 1;
-		total += events1;
+		total += event1;
 	}
 
-	if (document.forms['regform']['PPT'].checked) {
+	if (document.forms['regform']['PPT'].checked)
+	{
 		a = 1;
-		total += events2;
+		total += event2;
 	}
 
-	if (document.forms['regform']['OTH'].checked) {
+	if (document.forms['regform']['OTH'].checked)
+	{
 		a = 1;
-		total += events1;
+		total += event1;
 	}
 
-	if (document.getElementById('DA').checked) {
+	if (document.getElementById('DA').checked)
+	{
 		a = 1;
-		total += workshops1;
-	} else if (document.getElementById('BC').checked) {
+		total += workshop1;
+	} 
+	else if (document.getElementById('BC').checked)
+	{
 		a = 1;
-		total += workshops2;
-	} else if (document.getElementById('RPA').checked) {
+		total += workshop2;
+	}
+	else if (document.getElementById('RPA').checked)
+	{
 		a = 1;
-		total += workshops2;
-	} else if (document.getElementById('NL').checked) {
+		total += workshop2;
+	}
+	else if (document.getElementById('NL').checked)
+	{
 		a = 1;
-		total += workshops3;
+		total += workshop3;
 	}
 
-	if (a == 0) {
+	if (a == 0)
+	{
 		alert('Please select any workshops or events inorder to pay the amount!!!');
 		return false;
-	} //if a
+	}
+
 	alert('Total amount to be paid: ' + total);
-} //my
+}
